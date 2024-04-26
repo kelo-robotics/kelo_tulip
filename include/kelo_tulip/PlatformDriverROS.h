41ee4bea2a9fdcb88558582d85495efb9f155b70
Leonardo Tan <tan@locomotec.com>
2024-04-26 15:46:46 +0200

    change header to support ros2 foxy

--- include/kelo_tulip/PlatformDriverROS.h
+++ include/kelo_tulip/PlatformDriverROS.h
@@ -52,7 +52,7 @@
 #include "kelo_tulip/msg/shock_bin.hpp"
 #include "tf2_ros/transform_broadcaster.h"
 #include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
 #include <nav_msgs/msg/odometry.hpp>
 #include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/joy.hpp>
