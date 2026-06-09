fa5f1fb7d76efaf3ffc7808fa99f11e41407b433
Leonardo Tan <tan@locomotec.com>
2026-06-09 12:52:01 +0200

    fix compilation error for jazzy

--- include/kelo_tulip/PlatformDriverROS.h
+++ include/kelo_tulip/PlatformDriverROS.h
@@ -51,7 +51,7 @@
 #include "kelo_tulip/msg/kelo_drives_input.hpp"
 #include "tf2_ros/transform_broadcaster.h"
 #include "tf2/LinearMath/Quaternion.h"
-#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
 #include <nav_msgs/msg/odometry.hpp>
 #include <sensor_msgs/msg/imu.hpp>
 #include <sensor_msgs/msg/joy.hpp>
